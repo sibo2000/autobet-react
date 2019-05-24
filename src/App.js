06ae93e34aa641c91363d82fee02648afafa9575
Simon Bowler <simon@simonbowler.co.uk>
2019-05-24 16:40:08 +0100

    first commit, pull down predictions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      predictions: []
+    }
+  }
+
+  fetchPredictions = async () => {
+    try {
+      const res = await axios.get('https://ih551tgz9c.execute-api.eu-west-2.amazonaws.com/prod/predictions');
+      this.setState({predictions: JSON.parse(res.data.body)})
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  componentDidMount() {
+    this.fetchPredictions();
+  }
+
+  predictions() {
+    if (this.state.predictions.length) {
+      return this.state.predictions.map(item => {
+        console.log(item.home)
+        return <li>{item.home} v {item.away}</li>
+      })
+    }
+    return <li>No Predictions</li>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            {this.predictions()}
+            </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
